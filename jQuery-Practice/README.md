d2fad22ae4d07aad3dfc232fbb0a501e2c93a95e
lisaau <lisaau@users.noreply.github.com>
2020-01-28 15:01:20 -0800

    Complete jQuery assignment

---
+++ jQuery-Practice/README.md
@@ -0,0 +1,3 @@
+# jQuery Programming Fundamentals
+
+Exercise for Techtonica's [jQuery assignment](https://github.com/Techtonica/curriculum/blob/master/jquery/jquery.md).
---
+++ jQuery-Practice/static/js/myScript.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    // console.log('myScript.js loaded!');
+    $('#red-sentence').click(function() {
+      // First remove the purple-text class in case it was added first.
+      $(this).removeClass('purple-text');
+      // Then add the red-text class to turn the sentence red.
+      $(this).addClass('red-text');
+    });
+  
+    $('#blue-button').click(function() {
+      // We can also "chain" methods together like this:
+      $('#blue-sentence')
+      .removeClass('purple-text')
+      .addClass('blue-text');
+    });
+  
+    $('#purple-button').click(function() {
+      $('.colorful-sentence').addClass('purple-text');
+    });
+
+    $('#large-font-button').on('click', () => {
+        $('.colorful-sentence')
+        .removeClass('small-font')
+        .toggleClass('big-font');
+    })
+
+    $('#small-font-button').on('click', () => {
+        $('.colorful-sentence')
+        .removeClass('big-font')
+        .toggleClass('small-font');
+    })
+
+    $('.background-colorful-sentence').on('mouseenter', () => {
+        $('.background-colorful-sentence').toggleClass('background-color')
+    })
+  });
