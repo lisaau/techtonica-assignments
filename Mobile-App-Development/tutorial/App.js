3bfdc7af7daae7208b0f80a8051da40faf2209d0
lisaau <lisaau@users.noreply.github.com>
2020-04-08 16:27:06 -0700

    Creating a button

--- Mobile-App-Development/tutorial/App.js
+++ Mobile-App-Development/tutorial/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -9,6 +9,12 @@ export default function App() {
       <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -29,5 +35,14 @@ const styles = StyleSheet.create({
     color: '#888',
     fontSize: 18,
     marginHorizontal: 15,
+  }, 
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
   }, 
 });
