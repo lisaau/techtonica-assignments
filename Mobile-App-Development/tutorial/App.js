17c67ab34d761efa49994a0efe9bfa3dbe3520a4
lisaau <lisaau@users.noreply.github.com>
2020-04-08 11:19:45 -0700

    Add template expo cli files for tutorial

---
+++ Mobile-App-Development/tutorial/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>THERE'S SOMETHING ON THE SCREEN YAYY! 🎉🥳</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
